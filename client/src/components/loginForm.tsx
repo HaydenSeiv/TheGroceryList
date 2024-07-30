751080121f3884a792cb342dbf0a09aee757e28a
HaydenSiev <149506008+HaydenSiev@users.noreply.github.com>
2024-07-30 13:19:58 -0600

    -can now create user from login in screen

--- client/src/components/loginForm.tsx
+++ client/src/components/loginForm.tsx
@@ -1,37 +1,123 @@
-import { Stack, FormControl, FormLabel, Input, Box, Button, Container } from "@chakra-ui/react";
-import React from "react";
+import {
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+  Button,
+  Container,
+  Spinner,
+} from "@chakra-ui/react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { BASE_URL } from "../main";
+import { IoMdAdd } from "react-icons/io";
 
-export default function LoginForm(){
+export default function LoginForm() {
+  //state hook to create a new first name
+  const [newFirstName, setNewFirstName] = useState("");
 
-    return(
-        <Container>
-        <Stack display="flex" alignItems="center" justifyContent="center">
+  //state hook to asign a new Last name
+  const [newLastName, setNewLastName] = useState("");
+
+  //state hook to asign new email
+  const [newEmail, setNewEmail] = useState("");
+
+  const queryClient = useQueryClient();
+
+  //createItem function to update backend - uses TanStack useMutation hook
+  const { mutate: createUser, isPending: isCreating } = useMutation({
+    mutationKey: ["createUser"],
+
+    //the mutation function is async. e is of type of Formevent
+    mutationFn: async (e: React.FormEvent) => {
+      e.preventDefault();
+      try {
+        //we send the new items title,Category and CatID to the server and await for the response
+        const res = await fetch(BASE_URL + "/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          //update the body with JSON of new info
+          body: JSON.stringify({
+            firstName: newFirstName,
+            lastName: newLastName,
+            email: newEmail,
+          }),
+        });
+        const data = await res.json();
+
+        //if response not ok, throw error
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+
+        //reset the input boxs to be blank
+        setNewFirstName("");
+        setNewLastName("");
+        setNewEmail("");
+
+        return data;
+      } catch (error: any) {
+        throw new Error(error);
+      }
+    },
+
+    //onsuccess we invalidate the query to make sure nothing is fetched again or sent by accident as it has been completed and is now out of date
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["users"] });
+    },
+
+    onError: (error: any) => {
+      alert(error.message);
+    },
+  });
+  return (
+    <Container >
+        <form onSubmit={createUser}>
+      <Stack display="flex" alignItems="center" justifyContent="center">
         <FormControl isRequired>
-          <FormLabel>First name</FormLabel>
-          <Input placeholder="First name" />
+          <FormLabel textAlign={"center"}>First name</FormLabel>
+          <Input placeholder="First name" onChange={(e) => {setNewFirstName(e.target.value)}} />
         </FormControl>
         <FormControl isRequired>
-          <FormLabel>Last name</FormLabel>
-          <Input placeholder="Last name" />
+          <FormLabel textAlign={"center"}>Last name</FormLabel>
+          <Input placeholder="Last name" onChange={(e) => {setNewLastName(e.target.value)}} />
         </FormControl>
-        <FormControl>
-          <FormLabel>Email address</FormLabel>
-          <Input type="email" />
+        <FormControl isRequired>
+          <FormLabel textAlign={"center"}>Email address</FormLabel>
+          <Input type="email" placeholder="Email" onChange={(e) => {setNewEmail(e.target.value)}}/>
         </FormControl>
-      </Stack>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        width="100%"
-      >
-        <Stack>
-          <Button>
+
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          gap={3}
+        >
+          <Button
+           mx={2}
+           _active={{
+            transform: "scale(.97)",
+          }}>
             <Link to={`/list/`}>Log In</Link>
           </Button>
-        </Stack>
-      </Box>
-      </Container>
-    )
+          <Button
+            mx={2}
+            type="submit"
+            _active={{
+              transform: "scale(.97)",
+            }}
+          >
+        {/* if createUser function is running, show loading spinner  */}
+        {isCreating ? <Spinner size={"xs"} /> : <text> Create Account </text>}   
+          </Button>
+        </Box>
+      </Stack>
+      </form>
+    </Container>
+  );
 }
--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -6,10 +6,7 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import LoginForm from "../components/loginForm";
 
-export default function Root(props) {
-  const { loggedIn, email } = props;
-
-  const onButtonClick = () => {};
+export default function Root() {
 
   return (
     <Stack direction={`column`}>
